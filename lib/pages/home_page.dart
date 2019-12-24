ba080855607b014544858bc34a525e8326216720
lixiaoming02 <lixiaoming02@58.com>
2019-12-24 16:47:35 +0800

    轮播

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_shop/config/service_method.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -22,11 +26,98 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('百胜生活'),
+//        backgroundColor: Colors.grey,
+        appBar: AppBar(
+          title: Text('百胜生活'),
+        ),
+        body: FutureBuilder(
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              var data = json.decode(snapshot.data.toString());
+              List<Map> swiper = (data['data']['slides'] as List).cast();
+              List<Map> navigatorList = (data['data']['category'] as List).cast();
+              return Column(
+                children: <Widget>[
+                  SwiperDiy(
+                    swiperDateList: swiper,
+                  ),
+                  TopNavigator(navigatorList: navigatorList)
+                ],
+              );
+            } else {
+              return Center(
+                child: Text('加载中'),
+              );
+            }
+          },
+          future: getHomePageContent(),
+        ));
+  }
+}
+
+//首页轮播组件
+class SwiperDiy extends StatelessWidget {
+  final List swiperDateList;
+
+  SwiperDiy({Key key, this.swiperDateList}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print('设置的像素密度:${ScreenUtil.pixelRatio}');
+    print('设置的高:${ScreenUtil.screenHeight}');
+    print('设置的宽:${ScreenUtil.screenWidth}');
+
+    return Container(
+      height: ScreenUtil().setHeight(333),
+      width: ScreenUtil().setWidth(750),
+      child: Swiper(
+        itemCount: swiperDateList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network(
+            "${swiperDateList[index]['image']}",
+            fit: BoxFit.fill,
+          );
+        },
+        pagination: SwiperPagination(),
+        autoplay: true,
       ),
-      body: SingleChildScrollView(
-        child: Text(homePageContent),
+    );
+  }
+}
+
+class TopNavigator extends StatelessWidget {
+  final List navigatorList;
+
+  TopNavigator({this.navigatorList});
+
+  Widget _gridViewItemUI(BuildContext context, item) {
+    return InkWell(
+      onTap: () {
+        print('点击了导航');
+      },
+      child: Column(
+        children: <Widget>[
+          Image.network(
+            item['image'],
+            width: ScreenUtil().setWidth(95),
+          ),
+          Text(item['mallCategoryName'])
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: ScreenUtil().setHeight(320),
+      padding: EdgeInsets.all(3),
+      child: GridView.count(
+        crossAxisCount: 5,
+        padding: EdgeInsets.all(5.0),
+        children: navigatorList.map((item) {
+          return _gridViewItemUI(context, item);
+        }).toList(),
       ),
     );
   }
